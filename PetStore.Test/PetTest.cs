5618d341fb4b21e6eaa1e05da92e65cb45ea06e0
udeni.herath <udeni.herath@hotmail.com>
2024-05-05 08:11:59 +1000

    Test Case added : Api data test

--- PetStore.Test/PetTest.cs
+++ PetStore.Test/PetTest.cs
@@ -1,3 +1,8 @@
+using Microsoft.Extensions.Options;
+using Moq;
+using PetStore.App.Services;
+using PetStore.App.Utility;
+
 namespace PetStore.Test
 {
 	[TestClass]
@@ -6,6 +11,20 @@ namespace PetStore.Test
 		[TestMethod]
 		public async Task ShouldFindPetsByStatus()
 		{
+			var httpClientMock = new Mock<HttpClient>();
+
+			var apiAccessorMock = new Mock<IOptions<ApiAccessor>>();
+
+			apiAccessorMock.Setup(x => x.Value).Returns(new ApiAccessor { BaseUrl = AppSetting.GetBaseUrl() });
+
+			var petService = new PetService(httpClientMock.Object, apiAccessorMock.Object);
+
+			// Act
+			var actualPets = await petService.GetAvailablePetsAsync(PetStatusEnum.available.ToString());
+
+			// Assert
+			Assert.IsNotNull(actualPets);
+			Assert.IsTrue(actualPets.Count > 0);
 		}
 	}
 }
