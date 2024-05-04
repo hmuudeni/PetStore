40f0744e1e5ede702404cfb43d29d870a969e829
udeni.herath <udeni.herath@hotmail.com>
2024-05-05 07:15:30 +1000

    Model Classes Added

---
+++ PetStore.App/Models/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.App.Models
+{
+	public class Pet
+	{
+		public long Id { get; set; }
+		public Category Category { get; set; }
+		public string Name { get; set; }
+		public List<string> PhotoUrls { get; set; }
+		public List<Tag> Tags { get; set; }
+		public string Status { get; set; }
+	}
+}
