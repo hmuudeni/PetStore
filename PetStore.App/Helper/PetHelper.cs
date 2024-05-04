b81d15e52f055b415ec4b46cbf5b8d7adb01fa9b
udeni.herath <udeni.herath@hotmail.com>
2024-05-05 08:17:10 +1000

    pets sorted by category and displayed in reverse order by name

--- PetStore.App/Helper/PetHelper.cs
+++ PetStore.App/Helper/PetHelper.cs
@@ -17,7 +17,15 @@ namespace PetStore.App.Helper
 				.OrderBy(pet => pet.Category?.Name ?? "Unknown")
 				.ThenByDescending(p => p.Name);
 
-			
+			// Output to console
+			Console.WriteLine("Available pets sorted by category and displayed in reverse order by name:");
+			Console.WriteLine("---------------------------------------------------------------------------");
+			foreach (var pet in sortedPets)
+			{
+				string categoryName = pet.Category?.Name ?? "No Category";
+				Console.WriteLine($"Category: {categoryName}, Pet Name: {pet.Name}");
+			}
+
 		}
 	}
 }
