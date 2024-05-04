50a856ad12c0cef7ceba2a7766d53a6025fdc7ef
udeni.herath <udeni.herath@hotmail.com>
2024-05-05 07:18:41 +1000

    Utility Classes Added

---
+++ PetStore.App/Utility/AppSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.App.Utility
+{
+	public static class AppSetting
+	{
+		const string BASE_URL = "https://petstore.swagger.io/v2/";
+		public static string GetBaseUrl() { return BASE_URL; }
+	}
+}
