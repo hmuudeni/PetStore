ea278f6ba318278dfb4fb25b265aa652116fe96c
udeni.herath <udeni.herath@hotmail.com>
2024-05-05 07:27:36 +1000

    Service Interface and Main Method Added

---
+++ PetStore.App/Services/IPetService.cs
@@ -0,0 +1,14 @@
+﻿using PetStore.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.App.Services
+{
+	public interface IPetService
+	{
+		Task<List<Pet>> GetAvailablePetsAsync(string findByStatus);
+	}
+}
